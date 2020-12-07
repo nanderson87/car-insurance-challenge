ad05c408e5e54fd86ba9e6dddc3b93281ab6e55c
nanderson87 <nicolas.anderson@mercadolibre.com>
2020-12-07 14:47:24 -0300

    Add dependency mockito jupiter 2.23.4

---
+++ src/main/java/products/Product.java
@@ -0,0 +1,50 @@
+public abstract class  Product {
+    protected static final int MINIMUM_PRICE = 0;
+
+    private String name;
+    private int sellIn;
+    private int price;
+
+    public Product(String name, int sellIn, int price) {
+        this.name = name;
+        this.sellIn = sellIn;
+        this.price = price;
+    }
+
+    protected abstract int calculateNewPrice();
+
+    public void updatePrice() {
+        this.setPrice(this.calculateNewPrice());
+        this.updateSellIn();
+    }
+
+    public boolean saleDateExpired() {
+        return sellIn <= 0;
+    }
+
+    protected void updateSellIn() {
+        this.setSellIn(this.getSellIn() - 1);
+    }
+
+    protected void setSellIn(int sellIn) {
+        this.sellIn = sellIn;
+    }
+
+    protected void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getSellIn() {
+        return sellIn;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+
+}
